26d3d0e052b685ec04155ef385d08dcddfc01c4c
solozo <ozoemenamsolomon@yahoo.com>
2025-10-20 09:59:16 +0200

    Add job scribe functionality and update dependencies
    - Implemented JobScribeRequestDict for job data handling.
    - Added greet function to return a greeting message.
    - Introduced get_resume_pdf function to read and return PDF resume.
    - Updated .gitignore to include resume.pdf and .vscode/mcp.json.
    - Updated pyproject.toml and uv.lock to include pydantic dependency.

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+from typing import TypedDict, Dict
+from fastmcp import FastMCP, Context
+from pydantic import BaseModel
+# from fastmcp.resources import FileResource
+
+class JobScribeRequestDict(BaseModel):
+    id: str
+    name: str
+    company_name: str
+    company_address: Dict[str, str]
+    email: str
+
+mcp = FastMCP("job-scribe")
+
+@mcp.tool
+def greet(name: str) -> str:
+    return f"Hello, {name}!"
+
+""" """
+"""job: str, """
+        # uri="resource://{name}/resume_pdf",
+        # uri="resource://{job['company_name']}/{job['id']}",
+""" @mcp.resource(
+        uri="resource://resume_pdf",
+        name="ResumePDF",
+        mime_type="application/pdf",
+        description="A Generated PDF"
+)
+def get_resume_pdf() -> bytes:
+    with open("resume.pdf", "rb") as f:
+        pdf_bytes = f.read()
+    return pdf_bytes 
+"""
+"""Generates a PDF resume based on the provided resume data dictionary."""
+
 def main():
-    print("Hello from first-mcp!")
+    mcp.run()
 
 
 if __name__ == "__main__":
